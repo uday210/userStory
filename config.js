02adaf953bf40a992bdd26e5bdc0524aca3e934d
uday <udaykiran.uday@gmail.com>
2015-05-10 11:54:36 +0530

    adding all files

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  "database" :"mongodb://root:Bussas210#@ds061278.mongolab.com:61278/userstory",
+  "port" : process.env.PORT || 3000,
+  "secretKey" : "your secretKey"
+
+
+
+}
---
+++ public/app/app.routes.js
@@ -0,0 +1,39 @@
+angular.module('appRoutes',['ngRoute'])
+
+
+.config(function($routeProvider, $locationProvider){
+
+	$routeProvider
+
+		.when('/',{
+
+			templateUrl:'app/views/pages/home.html',
+			controller:'mainController',
+			controllerAs: 'main'
+		})
+
+		.when('/login',{
+
+			templateUrl:'app/views/pages/login.html'
+		})
+
+		.when('/signup',{
+			templateUrl:'app/views/pages/signup.html'
+		} )
+
+		.when('/allStories',{
+
+			templateUrl:'app/views/pages/allStories.html',
+			controller:'AllStoriesController',
+			controllerAs:'story',
+			resolve:{
+
+				stories: function(Story){
+					return Story.allStories();
+				}
+			}
+		})
+
+
+	$locationProvider.html5Mode(true);	
+})
